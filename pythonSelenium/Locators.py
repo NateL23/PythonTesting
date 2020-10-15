f1dd6d5836d64c4826d217963e0fd6bf28e2f7b6
Nate <natemac@Nathanaels-MBP.lan>
2020-10-15 02:56:01 -0400

    some more basic locator concepts using both css and xpath

--- pythonSelenium/Locators.py
+++ pythonSelenium/Locators.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 
 driver = webdriver.Chrome(executable_path='/Users/natemac/Documents/Repo/chromedriver')
 driver.get('https://www.rahulshettyacademy.com/angularpractice/')
@@ -10,9 +11,21 @@ driver.find_element_by_name('email').send_keys('nathanael.lemay23@gmail.com')
 driver.find_element_by_css_selector("input[name='name']").send_keys('Nate')
 # css
 
+dropdown = Select(driver.find_element_by_id('exampleFormControlSelect1'))
+# Select() provides methods to handle dropdown options
+dropdown.select_by_visible_text("Male")
+# select "Male" by visible text
+dropdown.select_by_index(1)
+# select 'Female' option using index
+'''
+$ dropdown.select_by_value()
+select using 'value' attribute
+'''
+
 # $ driver.find_element_by_xpath("//input[@type='submit']").click()
 driver.find_element_by_xpath("//*[contains(@type, 'submit')]").click()
 # xpath, click submit button as specific element, or using 'contains'
+# avoid xpath if possible, much slower than css
 
 # $ print(driver.find_element_by_class_name('alert-success').text)
 print(driver.find_element_by_css_selector("[class*='alert-success']").text)
---
+++ pythonSelenium/LocatorsExtension.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='/Users/natemac/Documents/Repo/chromedriver')
+driver.get('https://login.salesforce.com')
+
+driver.find_element_by_css_selector('#username').send_keys('nate')
+# css id
+driver.find_element_by_css_selector('.password').send_keys('password1')
+# css class
+driver.find_element_by_css_selector('.password').clear()
+# clear field
+driver.find_element_by_link_text('Forgot Your Password?').click()
+# css link text
+driver.find_element_by_xpath("//a[text() = 'Cancel']").click()
+# using 'a' as tagname (tagname is the first word in the DOM structure ('a', 'input', 'button', etc.)
+
+print(driver.find_element_by_xpath("//form[@name = 'login']/div[1]/label").text)
+# xpath parent/child tags used to print text above Username field
+print(driver.find_element_by_css_selector("form[name = 'login'] label:nth-child(3)").text)
+# css parent/child tags used to print text above Password field
+driver.close()
