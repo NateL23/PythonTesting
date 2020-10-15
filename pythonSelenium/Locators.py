66ba72849b428bf040ab98c1eea2aba21a4f47b7
Nate <natemac@Nathanaels-MBP.lan>
2020-10-14 22:22:52 -0400

    pushing updates to locators, catching up main branch to current, will branch off main moving forward

--- pythonSelenium/Locators.py
+++ pythonSelenium/Locators.py
@@ -3,13 +3,20 @@ from selenium import webdriver
 driver = webdriver.Chrome(executable_path='/Users/natemac/Documents/Repo/chromedriver')
 driver.get('https://www.rahulshettyacademy.com/angularpractice/')
 
-# driver.find_element_by_name('name').send_keys('Rahul')
 driver.find_element_by_id('exampleCheck1').click()
-driver.find_element_by_css_selector("input[name='name']").send_keys('rahul')
-driver.find_element_by_name('email').send_keys('Shetty')
+# id
+driver.find_element_by_name('email').send_keys('nathanael.lemay23@gmail.com')
+# name
+driver.find_element_by_css_selector("input[name='name']").send_keys('Nate')
 # css
-driver.find_element_by_xpath("//input[@type='submit']").click()
-# xpath
+
+# $ driver.find_element_by_xpath("//input[@type='submit']").click()
+driver.find_element_by_xpath("//*[contains(@type, 'submit')]").click()
+# xpath, click submit button as specific element, or using 'contains'
+
+# $ print(driver.find_element_by_class_name('alert-success').text)
+print(driver.find_element_by_css_selector("[class*='alert-success']").text)
+# print text displayed inside the element with class name 'alert-success'
+# use '*' as 'contains()'
 
 driver.close()
-
